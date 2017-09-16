41bd790ce8c458c85707f9e35d719f1efe28c12c
Kai Carver <kaicarver@gmail.com>
2017-09-17 07:41:35 +0800

    prepare array of time codes for non-delay-based stepper

--- Arduino/StepperTest/StepperTest.ino
+++ Arduino/StepperTest/StepperTest.ino
@@ -4,7 +4,50 @@
 
 // circuit: Red to Ground, Orange to D2, Yellow to D3
 
+unsigned long slitCmds[20];
+const int interval = 300;
+int count = 0;
+unsigned long curTime = 0;
+
+void setupTimes() {
+  // Morse code S O S
+  dot(); dot(); dot();    letterSpace(); // S
+  dash(); dash(); dash(); letterSpace(); // O
+  dot(); dot(); dot();    letterSpace(); // S
+  wordSpace();
+  Serial.print("number of offsets: ");
+  Serial.println(count);
+  for (int i = 0; i < count; i++) {
+    Serial.println(slitCmds[i]);
+  }
+}
+
+void dot() {
+  paintSymbol(1);
+}
+void dash() {
+  paintSymbol(3);
+}
+void paintSymbol(int duration) {
+  slitCmds[count] = curTime; count++; 
+  curTime += interval * duration;
+  slitCmds[count] = curTime; count++; 
+  symbolSpace();
+}
+void symbolSpace() {
+  curTime += interval * 1;
+}
+void letterSpace() {
+  curTime += interval * 2;
+}
+void wordSpace() {
+  curTime += interval * 4;
+}
+
 void setup() {
+  // initialize the serial port:
+  Serial.begin(9600);
+  setupTimes();
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
 }
