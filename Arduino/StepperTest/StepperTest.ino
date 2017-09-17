402bbb8545de446f7f98735e0c31f6e44415be39
Kai Carver <kaicarver@gmail.com>
2017-09-17 11:24:00 +0800

    no delay painting works

--- Arduino/StepperTest/StepperTest.ino
+++ Arduino/StepperTest/StepperTest.ino
@@ -4,25 +4,29 @@
 
 // circuit: Red to Ground, Orange to D2, Yellow to D3
 
-unsigned long slitCmds[20];
-const int interval = 300;
-int count = 0;
-unsigned long curTime = 0;
+const int maxCmds = 300; // Arduino Nano size limits us to no more than 300 or so long commands, 600 int
+unsigned long slitCmds[maxCmds]; // TODO: this size should be checked for
 
+const int interval = 200; // the basic element of time in milliseconds
+int count = 0; // this counts the commands
+unsigned long curTime = 0; // just used to calculate times for array
+
+// this is just testing code to fill the array with a pattern
+// the actual pattern to be painted should be calculated upstream
 void setupTimes() {
-  // Morse code S O S
-  dot(); dot(); dot();    letterSpace(); // S
-  dash(); dash(); dash(); letterSpace(); // O
-  dot(); dot(); dot();    letterSpace(); // S
-  wordSpace();
-  Serial.print("number of offsets: ");
+  Serial.print("basic interval of time in milliseconds: ");
+  Serial.println(interval);
+  Serial.println("setup array of timed on/off commands...");
+  // print SOS twice
+  morseSOS();
+  morseSOS();
+  Serial.print("number of commands: ");
   Serial.println(count);
+  
   for (int i = 0; i < count; i++) {
-    Serial.println(slitCmds[i]);
+    //Serial.println(slitCmds[i]);
   }
-  curTime = 0; // reset timer to use when printing
 }
-
 void dot() {
   paintSymbol(1);
 }
@@ -30,10 +34,14 @@ void dash() {
   paintSymbol(3);
 }
 void paintSymbol(int duration) {
-  slitCmds[count] = curTime; count++; 
-  curTime += interval * duration;
-  slitCmds[count] = curTime; count++; 
-  symbolSpace();
+  if (count < maxCmds - 2) {
+    slitCmds[count] = curTime; count++; 
+    curTime += interval * duration;
+    slitCmds[count] = curTime; count++; 
+    symbolSpace();
+  } else {
+    Serial.println("ignoring, max number of commands exceeded!");
+  }
 }
 void symbolSpace() {
   curTime += interval * 1;
@@ -44,84 +52,64 @@ void letterSpace() {
 void wordSpace() {
   curTime += interval * 4;
 }
+void morseSOS() {
+  // Morse code S O S
+  dot(); dot(); dot();    letterSpace(); // S
+  dash(); dash(); dash(); letterSpace(); // O
+  dot(); dot(); dot();    letterSpace(); // S
+  wordSpace();
+}
+
+// This is the printing code:
+// paint according to an array of on/off timecodes
 
 void setup() {
   // initialize the serial port:
   Serial.begin(9600);
-  setupTimes();
+  setupTimes(); // this is only for testing: the pattern to be painted should be calculated upstream
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
 }
 
+int slitCmdIndex = 0;
+
+unsigned long tInitial = millis();
+
+void loop() {
+  unsigned long t = millis() - tInitial; // relative time
+  if (slitCmdIndex < count) {
+    if (t >= slitCmds[slitCmdIndex]) {
+      if ((slitCmdIndex % 2) == 0) {
+        stepOn();  // turn paint on if not already on
+      } else {
+        stepOff(); // turn off if not already off
+      }
+      slitCmdIndex++;
+    }
+  } else {
+    // we executed all the commands in the array
+    Serial.println("done printing!");
+    delay(60000);
+  }
+}
+
 void stepOn() {
+  logCount();
+  Serial.println("step ON");
   digitalWrite(2,  LOW);
   digitalWrite(3, HIGH);  
 }
 void stepOff() {
+  logCount();
+  Serial.println("step OFF");
   digitalWrite(2, HIGH);
   digitalWrite(3,  LOW);
 }
 
-void loop() {
-  unsigned long currentMillis = millis();
-  // Morse code S O S
-  // S
-  stepOn();
-  delay(300);
-  stepOff();
-  delay(300);
-  
-  stepOn();
-  delay(300);
-  stepOff();
-  delay(300);
-  
-  stepOn();
-  delay(300);
-  stepOff();
-  delay(300);
-
-  // space between letters
-  delay(600);
-
-  // O
-  stepOn();
-  delay(900);
-  stepOff();
-  delay(300);
-  
-  stepOn();
-  delay(900);
-  stepOff();
-  delay(300);
-  
-  stepOn();
-  delay(900);
-  stepOff();
-  delay(300);
-
-  // space between letters
-  delay(600);
-
-  // S
-  stepOn();
-  delay(300);
-  stepOff();
-  delay(300);
-  
-  stepOn();
-  delay(300);
-  stepOff();
-  delay(300);
-  
-  stepOn();
-  delay(300);
-  stepOff();
-  delay(300);
-
-  // space between letters
-  delay(600);
-  
-  // space between words
-  delay(1200);
+void logCount() {
+  Serial.print(slitCmdIndex+1);
+  Serial.print("/");
+  Serial.print(count);
+  Serial.print(": ");
 }
+
